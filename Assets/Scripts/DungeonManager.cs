15ccbe9acdf05762092a3c2de685070fcd35a865
ZHIMAHOO <emsenjones@gmail.com>
2025-05-06 20:33:58 +0800

    Coding change test.

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -2,26 +2,5 @@ using UnityEngine;
 
 public class DungeonManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    [SerializeField] private GameObject dungeonPrefab;
 
-    void Start()
-    {
-        
-        
-        
-        
-    }
-
-    private void Initialize()
-    {
-        if (!dungeonPrefab) return;
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
